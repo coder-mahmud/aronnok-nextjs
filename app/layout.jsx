afafc7271a900035536c3084087964ed37f9328f
coder-mahmud <mahmud.linked@gmail.com>
2026-06-22 09:40:54 +0600

    Facebook pixel code added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
+import Script from "next/script";
 
 import { Cinzel } from 'next/font/google';
 import Footer from "@/components/Footer";
@@ -35,6 +36,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={cinzel.variable}>
+      <head>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '876441661625346');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+
+
+
       <body
         className={`antialiased`}
       > 
